c4126e17c334dd9bc33e8ed0d4dee4e6447cefd6
易墨 <1280160831@qq.com>
2018-06-26 08:44:11 +0800

    Update scw.js

--- modules/scw.js
+++ modules/scw.js
@@ -42,7 +42,7 @@ const exit_code = 'close browser'
 
 async function start(params) {
   const browser = await puppeteer.launch({
-    args: ['--no-sandbox'],
+    args: ['--no-sandbox','--disable-setuid-sandbox'],
     // 若是手动下载的chromium需要指定chromium地址, 默认引用地址为 /项目目录/node_modules/puppeteer/.local-chromium/
     // executablePath: exepath,
     //设置超时时间
